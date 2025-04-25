18a2983ff5792c379ce8bf511f07e32004a1dd86
druv <druv@druv.druv.prinzpiuz.in>
2025-04-25 12:14:12 +0530

    fix: minor fixes

---
+++ down_and_up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "$(date)"
+
+COMPOSE_DIR="/home/druv/druv_setup"
+
+cd "$COMPOSE_DIR" || { echo "Error: Could not change to directory $COMPOSE_DIR"; exit 1; }
+
+echo "Stopping Docker Compose services..."
+
+docker compose down
+
+sleep 5
+
+echo "Starting Docker Compose services..."
+
+docker-compose --env-file=.env up -d --remove-orphans --build --force-recreate
+
+echo "Docker Compose services down and restarted."
