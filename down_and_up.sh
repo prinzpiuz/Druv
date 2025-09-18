32c049edeebb5ac8206b3efd4550840c24193e73
druv <druv@druv.druv.prinzpiuz.in>
2025-09-18 12:59:28 +0530

    fix: subnet creation added with network

--- down_and_up.sh
+++ down_and_up.sh
@@ -14,6 +14,8 @@ sleep 5
 
 echo "Starting Docker Compose services..."
 
+docker network create --subnet=172.18.0.0/24 arr
+
 docker-compose --env-file=.env up -d --remove-orphans --build --force-recreate
 
 echo "Docker Compose services down and restarted."
