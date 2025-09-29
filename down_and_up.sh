591c768cff4f5e6e4be02ef009c9b088f541153f
druv <druv@druv.druv.prinzpiuz.in>
2025-09-29 12:00:30 +0530

    feat: system prune made as optional

--- down_and_up.sh
+++ down_and_up.sh
@@ -14,7 +14,12 @@ sleep 5
 
 echo "Starting Docker Compose services..."
 
-docker network create --subnet=172.18.0.0/24 arr
+if [ "$1" == "--all" ]; then
+    echo "Rebuilding all services..."
+    docker system prune -a -f
+    docker network create --subnet=172.18.0.0/24 arr
+fi
+
 
 docker-compose --env-file=.env up -d --remove-orphans --build --force-recreate
 
